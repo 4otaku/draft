ffd5d52d36a7a51a8b09fe3445e0ad1c0bef1ed2
Nameless <admin@4otaku.ru>
2012-06-23 17:15:30 +0400

    import

--- libs/transform/upload_mtg.php
+++ libs/transform/upload_mtg.php
@@ -21,11 +21,15 @@ class Transform_Upload_Mtg extends Transform_Upload_Abstract_Image
 		$newthumb = IMAGES.SL.'small'.$pathinfo['dirname'].SL.$pathinfo['filename'].'.jpg';
 
 		chmod($this->file, 0755);
-
-		copy($this->file, IMAGES.SL.'full'.SL.$this->name);
-
 		$this->worker = Transform_Image::get_worker($this->file);
 		$this->animated = false;
+
+		if ($this->info[0] > Config::get('card', 'full_width')) {
+			$this->scale(array(Config::get('card', 'full_width'), Config::get('card', 'full_height')),
+				IMAGES.SL.'full'.SL.$this->name);
+		} else {
+			copy($this->file, IMAGES.SL.'full'.SL.$this->name);
+		}
 
 		$this->scale(array(Config::get('card', 'width'), Config::get('card', 'height')), $newthumb);
 	}
