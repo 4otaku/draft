6d8c6359812f08ef3c50ecd521f2598dbe89f2c6
Nameless <admin@4otaku.ru>
2012-11-27 00:43:51 +0400

    fix

--- libs/booster/mythic.php
+++ libs/booster/mythic.php
@@ -2,6 +2,9 @@
 
 class Booster_Mythic extends Booster_Abstract_New
 {
+	protected $land = 1;
+	protected $common = 10;
+
 	protected function add_card($rarity) {
 		if ($rarity == 3 && (mt_rand(0, 8) < 1)) {
 			$rarity = 4;
